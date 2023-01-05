fcd77da6bd5af06e2ae24ea20994ba854d0dadcc
miield <aodasola95@gmail.com>
2023-01-05 19:51:43 +0100

    first npm package built

---
+++ index.js
@@ -0,0 +1,10 @@
+function randomNoGenerator(min, max) {
+
+    if(typeof(max) !== 'number' && typeof(min) !== 'number') {
+        min = 0;  max = 1;
+    }
+   return (Math.random() * (max-min)) + min;
+
+}
+
+module.exports = randomNoGenerator;
